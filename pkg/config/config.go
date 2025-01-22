9b88e77e19c00cf56ef3d571b2aa27d452a4ccb0
bdarge <bin.darge@gmail.com>
2025-01-21 23:27:06 -0500

    change style

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -5,14 +5,14 @@ import "github.com/spf13/viper"
 
 // Config app configuration
 type Config struct {
-	ServerPort                  string `mapstructure:"PORT"`
-	DSN                   string `mapstructure:"DSN"`
-	TokenSecretKey        string `mapstructure:"JWT_TOKEN_SECRET_KEY"`
-	TokenExpOn            int    `mapstructure:"JWT_TOKEN_EXP_ON"`
-	TokenIssuer           string `mapstructure:"JWT_TOKEN_ISSUER"`
-	RefreshTokenExpOn     int    `mapstructure:"JWT_REFRESH_TOKEN_EXP_ON"`
+	ServerPort string `mapstructure:"PORT"`
+	DSN string `mapstructure:"DSN"`
+	TokenSecretKey string `mapstructure:"JWT_TOKEN_SECRET_KEY"`
+	TokenExpOn int `mapstructure:"JWT_TOKEN_EXP_ON"`
+	TokenIssuer string `mapstructure:"JWT_TOKEN_ISSUER"`
+	RefreshTokenExpOn int `mapstructure:"JWT_REFRESH_TOKEN_EXP_ON"`
 	RefreshTokenSecretKey string `mapstructure:"JWT_REFRESH_TOKEN_SECRET_KEY"`
-	LogLevel							slog.Level    `mapstructure:"LOG_LEVEL"`
+	LogLevel slog.Level `mapstructure:"LOG_LEVEL"`
 }
 
 // LoadConfig loads config from files
